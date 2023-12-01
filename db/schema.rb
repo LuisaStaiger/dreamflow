5c33b29b1266e115d4cf213dd64e7218f6778f83
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-01 12:51:04 +0100

    Clean Up DB, Implement User-specific Greetings & Questions w/ UI Improvements in Views

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_132247) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_01_112653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "user_answer"
+    t.bigint "dream_id", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dream_id"], name: "index_answers_on_dream_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "dream_labels", force: :cascade do |t|
     t.bigint "dream_id", null: false
@@ -35,7 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_132247) do
 
   create_table "labels", force: :cascade do |t|
     t.string "name"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -46,7 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_132247) do
     t.datetime "updated_at", null: false
     t.string "question_text"
     t.text "explanation_text"
-    t.boolean "default"
+    t.boolean "correct"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
@@ -58,10 +67,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_132247) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "current_goal"
+    t.string "current_dreams"
+    t.string "current_influences"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "dreams"
+  add_foreign_key "answers", "questions"
   add_foreign_key "dream_labels", "dreams"
   add_foreign_key "dream_labels", "labels"
   add_foreign_key "dreams", "users"
