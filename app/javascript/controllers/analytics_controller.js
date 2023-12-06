bfcc2196fec537e1b8534989f14e556ff5268733
Rebeca Fernandez <rebeca.fernandez.per@gmail.com>
2023-12-06 11:54:07 +0100

    analytics-colors

--- app/javascript/controllers/analytics_controller.js
+++ app/javascript/controllers/analytics_controller.js
@@ -29,9 +29,14 @@ export default class extends Controller {
               label: 'Dreams Statistic',
               data: this.dataValue,
               backgroundColor: [
-                'rgb(216, 212, 242)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
+                //'rgb(216, 212, 242)',
+                //'rgb(54, 162, 235)',
+               // 'rgb(255, 205, 86)'
+                'rgb(239, 233, 247)',
+                'rgb(58, 34, 88)',
+                'rgb(106, 62, 161)',
+                'rgb(167, 121, 225)',
+                'rgb(200, 181, 224)'
               ],
               hoverOffset: 4
             }
